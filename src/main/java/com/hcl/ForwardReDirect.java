9a9c54566656fb74f7dcaa0e34c1a19f805745b2
hclsaran <renusaran@hotmail.com>
2022-07-05 11:55:08 -0400

    latest updates3

--- src/main/java/com/hcl/ForwardReDirect.java
+++ src/main/java/com/hcl/ForwardReDirect.java
@@ -10,27 +10,13 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
-public class SessionServlet extends HttpServlet {
+public class ForwardReDirect extends HttpServlet {
 	
 	public void doGet(HttpServletRequest req,HttpServletResponse res) throws ServletException,IOException{
 		res.setContentType("texthtml");
 		PrintWriter pw=res.getWriter();
-		String username=req.getParameter("uname");
-		String password=req.getParameter("pwd");
-		if(username.equals("Julian")&& password.equals("123")) {
-			pw.println("Welcome "+username);
-			HttpSession session=req.getSession();
-			session.setAttribute("name", username);
-		}else {
-			pw.println("Invalid Username/Password");
-			req.getRequestDispatcher("session.jsp").include(req, res);
-			//include and forward
-		}
-		
-		 
-		
-		
-		
+		// res.sendRedirect("https://www.google.com/");
+		req.getRequestDispatcher("https://www.google.com/").forward(req, res);
 		pw.close();//close the stream
 		
 		
@@ -39,5 +25,5 @@ public class SessionServlet extends HttpServlet {
 	
 	public void doPost(HttpServletRequest req,HttpServletResponse res) throws ServletException,IOException{
       doGet(req,res);
-}
+	}
 }
